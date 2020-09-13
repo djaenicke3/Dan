b7e34f835c3d4c54ea13fe851fdebd20c5f3cfcd
bouzir-dev <mohamedkhalilbouzir@insat.u-carthage.tn>
2020-09-13 14:35:27 +0100

    'version1.1'

--- Dan/Dan/settings.py
+++ Dan/Dan/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'News',
         'USER':'postgres',
-        'PASSWORD':'admin',
+        'PASSWORD':'Redbox19!',
         'HOST':'localhost',
         'PORT':'5432',
     }
